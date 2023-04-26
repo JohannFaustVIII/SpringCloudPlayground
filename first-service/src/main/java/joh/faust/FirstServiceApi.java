ea9206b4986628fb388ff1e18fe7299e204e985b
Faust VIII <joh.faustviii@gmail.com>
2023-04-26 11:51:11 +0200

    Add random id to get different responses from different instances of FirstService

--- first-service/src/main/java/joh/faust/FirstServiceApi.java
+++ first-service/src/main/java/joh/faust/FirstServiceApi.java
@@ -1,13 +1,17 @@
 package joh.faust;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FirstServiceApi {
 
+    @Value("${eureka.instance.instanceId}")
+    private String instanceId;
+
     @GetMapping("/")
     public String getHello() {
-        return "First Service: Hello World!";
+        return instanceId + ": First Service: Hello World!";
     }
 }
