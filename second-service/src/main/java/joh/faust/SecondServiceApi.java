5862ecd8ef7974914a99d37c3b739fe902dc393b
Faust VIII <joh.faustviii@gmail.com>
2023-04-26 12:57:45 +0200

    Add configuration server

--- second-service/src/main/java/joh/faust/SecondServiceApi.java
+++ second-service/src/main/java/joh/faust/SecondServiceApi.java
@@ -1,13 +1,17 @@
 package joh.faust;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SecondServiceApi {
 
+    @Value("${example.property}")
+    private String exampleProperty;
+
     @GetMapping("/")
     public String getHello() {
-        return "Second Service: Hello World!";
+        return "Second Service: Hello World!\nExample property is: " + exampleProperty;
     }
 }
