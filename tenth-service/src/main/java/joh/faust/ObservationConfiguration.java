d04a95d246b43a90cbe3d078ea78d48e76ffc755
Faust VIII <joh.faustviii@gmail.com>
2023-05-25 12:37:04 +0200

    Send tenthService tracing to Jaeger

--- tenth-service/src/main/java/joh/faust/ObservationConfiguration.java
+++ tenth-service/src/main/java/joh/faust/ObservationConfiguration.java
@@ -2,6 +2,8 @@ package joh.faust;
 
 import io.micrometer.observation.ObservationRegistry;
 import io.micrometer.observation.aop.ObservedAspect;
+import io.opentelemetry.exporter.otlp.http.trace.OtlpHttpSpanExporter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,5 +13,12 @@ public class ObservationConfiguration {
     @Bean
     public ObservedAspect observedAspect(ObservationRegistry observationRegistry) {
         return new ObservedAspect(observationRegistry);
+    }
+
+    @Bean
+    OtlpHttpSpanExporter otlpHttpSpanExporter(@Value("${management.otlp.tracing.endpoint}") String url) {
+        return OtlpHttpSpanExporter.builder()
+                .setEndpoint(url)
+                .build();
     }
 }
