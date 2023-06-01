80333513738abb48a825727a1e6de0d88b3a4c53
Faust VIII <joh.faustviii@gmail.com>
2023-06-01 15:57:05 +0200

    WIP Add Dockerfiles

---
+++ docker-build-images.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+services=("first" "second" "third" "fourth" "fifth" "sixth" "seventh" "eighth" "ninth" "tenth" "eleventh")
+others=("configuration-server" "discovery" "gateway")
+
+for d in ${services[*]};
+do
+  (cd "$d"-service && docker build -t "$d"-local .)
+done
+
+for d in ${others[*]};
+do
+  (cd $d && docker build -t "$d"-local .)
+done
