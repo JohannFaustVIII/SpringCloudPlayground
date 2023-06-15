5a45c9f670fbe659a6ab38e21d42503882df3a2a
Faust VIII <joh.faustviii@gmail.com>
2023-06-15 11:42:58 +0200

    Get sixth service URL from Eureka in fifth service

--- fifth-service/src/main/java/joh/faust/CBService.java
+++ fifth-service/src/main/java/joh/faust/CBService.java
@@ -1,5 +1,7 @@
 package joh.faust;
 
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.EurekaClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.circuitbreaker.CircuitBreaker;
 import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
@@ -9,19 +11,22 @@ import org.springframework.web.client.RestTemplate;
 @Service
 public class CBService {
 
-    @Value("${sixService.url}")
-    private String gatewayUrl;
+    @Value("${sixService.name}")
+    private String serviceName;
 
     private final RestTemplate restTemplate;
     private final CircuitBreaker circuitBreaker;
+    private final EurekaClient eurekaClient;
 
-    public CBService(CircuitBreakerFactory circuitBreakerFactory) {
+    public CBService(CircuitBreakerFactory circuitBreakerFactory, EurekaClient eurekaClient) {
         this.circuitBreaker = circuitBreakerFactory.create("customCircuitBreaker");
+        this.eurekaClient = eurekaClient;
         this.restTemplate = new RestTemplate();
     }
 
     public String getUuid() {
-        String url = gatewayUrl + "/uuid";
+        InstanceInfo instanceInfo = eurekaClient.getApplication(serviceName).getInstances().get(0);
+        String url = instanceInfo.getHomePageUrl() + "/uuid";
 
         return circuitBreaker.run( () -> restTemplate.getForObject(url, String.class), throwable -> "none");
     }
