d68d419595eb12180f50e79b91e13eae2186204f
Faust VIII <joh.faustviii@gmail.com>
2023-07-04 13:24:22 +0200

    Count calls to endpoint of first service
    To observe load balancing

--- first-service/src/main/java/joh/faust/FirstServiceApi.java
+++ first-service/src/main/java/joh/faust/FirstServiceApi.java
@@ -7,11 +7,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class FirstServiceApi {
 
+    private int calls = 0;
+
     @Value("${eureka.instance.instanceId}")
     private String instanceId;
 
     @GetMapping("/")
     public String getHello() {
-        return instanceId + ": First Service: Hello World!";
+        return instanceId + ": First Service: Hello World! Number of calls: " + (++calls);
     }
 }
