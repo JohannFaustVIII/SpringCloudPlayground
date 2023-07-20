e832bc0dc01592c14b046331dab37c43780b8f12
Faust VIII <joh.faustviii@gmail.com>
2023-07-20 15:13:24 +0200

    WIP Add EventSourcing
    Change idea how to handle event sourcing

--- fourteenth-service/src/main/java/joh/faust/event/Event.java
+++ fourteenth-service/src/main/java/joh/faust/event/Event.java
@@ -7,7 +7,9 @@ public abstract class Event {
 
     // TODO: below should be in event entity? maybe
     // TODO: how to handle successful insert/update/delete? Transactional and then create event if successful?
-    // TODO: ^^^ need to keep database consistency
+    // TODO: the idea has changed: keep the state of a single object as aggregate, apply changes by generating events,
+    // TODO: increase versions with each change, save snapshots to db to keep reloading easier, and reapply events when
+    // TODO: reload from the db
 
     public final UUID id = UUID.randomUUID();
     public final Date created = new Date();
