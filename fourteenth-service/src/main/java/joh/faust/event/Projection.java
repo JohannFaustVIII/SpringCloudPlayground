897a86eb5d7b63f2c7b130e6923c5bc510742181
Faust VIII <joh.faustviii@gmail.com>
2023-08-22 15:10:57 +0200

    WIP Write idea about projection

---
+++ fourteenth-service/src/main/java/joh/faust/event/Projection.java
@@ -0,0 +1,9 @@
+package joh.faust.event;
+
+public abstract class Projection {
+
+    // TODO: it can be really similar to aggregate, is it worth making?
+    //  as for CQRS - yes, but for KISS - it doesn't look exactly right
+    //  TO THINK FURTHER
+
+}
